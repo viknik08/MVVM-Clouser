a02d9cb157c51743b888f0369f8ff66137eb2331
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-09-15 16:38:08 +0800

    Initial Commit

---
+++ MVVM + Clouser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM + Clouser
+//
+//  Created by Виктор Басиев on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
